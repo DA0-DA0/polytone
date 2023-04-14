681b1806e58f502d0d9e9215d75d34fdbd7040a8
Art3mix <r3mix.il@gmail.com>
2023-04-14 14:02:14 +0300

    fmt/clippy

--- contracts/note/src/tests.rs
+++ contracts/note/src/tests.rs
@@ -1,156 +1,154 @@
-#[cfg(test)]
-mod tests {
-    use cosmwasm_std::{
-        testing::{mock_dependencies, mock_env, mock_info},
-        to_binary, Uint64, WasmMsg,
-    };
 
-    use crate::{
-        contract::{execute, instantiate},
-        error::ContractError,
-        msg::InstantiateMsg,
-        state::CHANNEL,
-    };
+use cosmwasm_std::{
+    testing::{mock_dependencies, mock_env, mock_info},
+    to_binary, Uint64, WasmMsg,
+};
 
-    #[test]
-    fn simple_note() {
-        let mut deps = mock_dependencies();
-        let env = mock_env();
-        let info = mock_info("sender", &[]);
+use crate::{
+    contract::{execute, instantiate},
+    error::ContractError,
+    msg::InstantiateMsg,
+    state::CHANNEL,
+};
 
-        instantiate(
-            deps.as_mut(),
-            env.clone(),
-            info.clone(),
-            InstantiateMsg {
-                pair: None,
-                controller: None,
-            },
-        )
-        .unwrap();
-        CHANNEL
-            .save(deps.as_mut().storage, &"some_channel".to_string())
-            .unwrap();
+#[test]
+fn simple_note() {
+    let mut deps = mock_dependencies();
+    let env = mock_env();
+    let info = mock_info("sender", &[]);
 
-        execute(
-            deps.as_mut(),
-            env.clone(),
-            info.clone(),
-            crate::msg::ExecuteMsg::Execute {
-                on_behalf_of: None,
-                msgs: vec![WasmMsg::Execute {
-                    contract_addr: "some_addr".to_string(),
-                    msg: to_binary("some_msg").unwrap(),
-                    funds: vec![],
-                }
-                .into()],
-                callback: None,
-                timeout_seconds: Uint64::new(10000),
-            },
-        )
+    instantiate(
+        deps.as_mut(),
+        env.clone(),
+        info.clone(),
+        InstantiateMsg {
+            pair: None,
+            controller: None,
+        },
+    )
+    .unwrap();
+    CHANNEL
+        .save(deps.as_mut().storage, &"some_channel".to_string())
         .unwrap();
 
-        // note is controlled, but `on_behalf_of` is set should error
-        let err = execute(
-            deps.as_mut(),
-            env,
-            info,
-            crate::msg::ExecuteMsg::Execute {
-                on_behalf_of: Some("some_addr".to_string()),
-                msgs: vec![WasmMsg::Execute {
-                    contract_addr: "some_addr".to_string(),
-                    msg: to_binary("some_msg").unwrap(),
-                    funds: vec![],
-                }
-                .into()],
-                callback: None,
-                timeout_seconds: Uint64::new(10000),
-            },
-        )
-        .unwrap_err();
+    execute(
+        deps.as_mut(),
+        env.clone(),
+        info.clone(),
+        crate::msg::ExecuteMsg::Execute {
+            on_behalf_of: None,
+            msgs: vec![WasmMsg::Execute {
+                contract_addr: "some_addr".to_string(),
+                msg: to_binary("some_msg").unwrap(),
+                funds: vec![],
+            }
+            .into()],
+            callback: None,
+            timeout_seconds: Uint64::new(10000),
+        },
+    )
+    .unwrap();
 
-        assert_eq!(err, ContractError::NotControlledButOnBehalfIsSet)
-    }
+    // note is controlled, but `on_behalf_of` is set should error
+    let err = execute(
+        deps.as_mut(),
+        env,
+        info,
+        crate::msg::ExecuteMsg::Execute {
+            on_behalf_of: Some("some_addr".to_string()),
+            msgs: vec![WasmMsg::Execute {
+                contract_addr: "some_addr".to_string(),
+                msg: to_binary("some_msg").unwrap(),
+                funds: vec![],
+            }
+            .into()],
+            callback: None,
+            timeout_seconds: Uint64::new(10000),
+        },
+    )
+    .unwrap_err();
 
-    #[test]
-    fn controlled_note() {
-        let mut deps = mock_dependencies();
-        let env = mock_env();
-        let info = mock_info("sender", &[]);
-        let controller_info = mock_info("controller", &[]);
+    assert_eq!(err, ContractError::NotControlledButOnBehalfIsSet)
+}
 
-        instantiate(
-            deps.as_mut(),
-            env.clone(),
-            info.clone(),
-            InstantiateMsg {
-                pair: None,
-                controller: Some("controller".to_string()),
-            },
-        )
+#[test]
+fn controlled_note() {
+    let mut deps = mock_dependencies();
+    let env = mock_env();
+    let info = mock_info("sender", &[]);
+    let controller_info = mock_info("controller", &[]);
+
+    instantiate(
+        deps.as_mut(),
+        env.clone(),
+        info.clone(),
+        InstantiateMsg {
+            pair: None,
+            controller: Some("controller".to_string()),
+        },
+    )
+    .unwrap();
+    CHANNEL
+        .save(deps.as_mut().storage, &"some_channel".to_string())
         .unwrap();
-        CHANNEL
-            .save(deps.as_mut().storage, &"some_channel".to_string())
-            .unwrap();
 
-        execute(
-            deps.as_mut(),
-            env.clone(),
-            controller_info.clone(),
-            crate::msg::ExecuteMsg::Execute {
-                on_behalf_of: Some("some_addr".to_string()),
-                msgs: vec![WasmMsg::Execute {
-                    contract_addr: "some_addr".to_string(),
-                    msg: to_binary("some_msg").unwrap(),
-                    funds: vec![],
-                }
-                .into()],
-                callback: None,
-                timeout_seconds: Uint64::new(10000),
-            },
-        )
-        .unwrap();
+    execute(
+        deps.as_mut(),
+        env.clone(),
+        controller_info.clone(),
+        crate::msg::ExecuteMsg::Execute {
+            on_behalf_of: Some("some_addr".to_string()),
+            msgs: vec![WasmMsg::Execute {
+                contract_addr: "some_addr".to_string(),
+                msg: to_binary("some_msg").unwrap(),
+                funds: vec![],
+            }
+            .into()],
+            callback: None,
+            timeout_seconds: Uint64::new(10000),
+        },
+    )
+    .unwrap();
 
-        // note is controlled but `on_behalf_of` is not set, should error
-        let err = execute(
-            deps.as_mut(),
-            env.clone(),
-            controller_info,
-            crate::msg::ExecuteMsg::Execute {
-                on_behalf_of: None,
-                msgs: vec![WasmMsg::Execute {
-                    contract_addr: "some_addr".to_string(),
-                    msg: to_binary("some_msg").unwrap(),
-                    funds: vec![],
-                }
-                .into()],
-                callback: None,
-                timeout_seconds: Uint64::new(10000),
-            },
-        )
-        .unwrap_err();
+    // note is controlled but `on_behalf_of` is not set, should error
+    let err = execute(
+        deps.as_mut(),
+        env.clone(),
+        controller_info,
+        crate::msg::ExecuteMsg::Execute {
+            on_behalf_of: None,
+            msgs: vec![WasmMsg::Execute {
+                contract_addr: "some_addr".to_string(),
+                msg: to_binary("some_msg").unwrap(),
+                funds: vec![],
+            }
+            .into()],
+            callback: None,
+            timeout_seconds: Uint64::new(10000),
+        },
+    )
+    .unwrap_err();
 
-        assert_eq!(err, ContractError::OnBehalfOfNotSet);
+    assert_eq!(err, ContractError::OnBehalfOfNotSet);
 
-        // note is controlled but not controller called execute, should error
-        let err = execute(
-            deps.as_mut(),
-            env,
-            info,
-            crate::msg::ExecuteMsg::Execute {
-                on_behalf_of: Some("some_addr".to_string()),
-                msgs: vec![WasmMsg::Execute {
-                    contract_addr: "some_addr".to_string(),
-                    msg: to_binary("some_msg").unwrap(),
-                    funds: vec![],
-                }
-                .into()],
-                callback: None,
-                timeout_seconds: Uint64::new(10000),
-            },
-        )
-        .unwrap_err();
+    // note is controlled but not controller called execute, should error
+    let err = execute(
+        deps.as_mut(),
+        env,
+        info,
+        crate::msg::ExecuteMsg::Execute {
+            on_behalf_of: Some("some_addr".to_string()),
+            msgs: vec![WasmMsg::Execute {
+                contract_addr: "some_addr".to_string(),
+                msg: to_binary("some_msg").unwrap(),
+                funds: vec![],
+            }
+            .into()],
+            callback: None,
+            timeout_seconds: Uint64::new(10000),
+        },
+    )
+    .unwrap_err();
 
-        assert_eq!(err, ContractError::NotController)
-    }
+    assert_eq!(err, ContractError::NotController)
 }
