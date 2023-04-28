705fb2fa88b14d3b9e9b7beb57ac0a1ec6981376
Art3mix <r3mix.il@gmail.com>
2023-04-28 14:25:53 +0300

    final fix, i swear

--- devtools/optimize.sh
+++ devtools/optimize.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
 
-# workspace-optimizer only optimizes contracts in the `contracts/*`
-# folder, and needs to be invoked differently depending on the chip
-# archetecture, so this script exists to abstract that away.
-
-# this function is called when the script exits,
-# regardless of the reason (error, success, etc)
-function cleanup {
-  for folder in accessories/*
-  do
-    folder=${folder/'accessories/'}
-    rm -R contracts/$folder
-  done
-
-  # do cargo check to update (revert) lock file
-  cargo check
-}
-
-# register the cleanup function to be called on the EXIT signal
-trap cleanup EXIT
-
-# copy all the accessories contracts to contracts folder
-# we need this because workspace-optimizer is only compiling
-# contracts that are in the contracts folder.
-for folder in accessories/*
-do
-  folder=${folder/'accessories/'}
-  cp -R accessories/$folder contracts/$folder
-done
-
-# do cargo check to update lock file
-cargo check
-
 # Run workspace-optimizer
 if [[ $(uname -m) =~ "arm64" ]]; then \
     docker run --rm -v "$(pwd)":/code \
@@ -49,14 +17,5 @@ fi
 
 mkdir -p tests/wasms
 
-if [[ $(uname -m) =~ "arm64" ]]; then \
-    cp artifacts/polytone_note-aarch64.wasm tests/wasms/polytone_note.wasm
-    cp artifacts/polytone_voice-aarch64.wasm tests/wasms/polytone_voice.wasm
-    cp artifacts/polytone_tester-aarch64.wasm tests/wasms/polytone_tester.wasm
-    cp artifacts/polytone_proxy-aarch64.wasm tests/wasms/polytone_proxy.wasm
-else
-    cp artifacts/polytone_note.wasm tests/wasms/
-    cp artifacts/polytone_voice.wasm tests/wasms/
-    cp artifacts/polytone_tester.wasm tests/wasms/
-    cp artifacts/polytone_proxy.wasm tests/wasms/
-fi
+# move all wasm files to our tests folder
+cp -R ./artifacts/*.wasm tests/wasms
